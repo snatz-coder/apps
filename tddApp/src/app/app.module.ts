a040044ffa08acf050552dfaf85ad5cfd68e083e
snatz-coder <snazlopez@gmail.com>
2020-04-19 22:36:39 +0530

    created components

--- tddApp/src/app/app.module.ts
+++ tddApp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './src/components/header/header.component';
+import { NewsComponent } from './src/components/news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule
